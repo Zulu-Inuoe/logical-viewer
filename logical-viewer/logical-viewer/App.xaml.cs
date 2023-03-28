c82ff6a41fcabd007a43ba18416d4e015aa6a7c8
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2023-03-28 09:29:43 -0400

    .editor config and fixup line endings
    Previous Visual Studio just had an option for this in preferences

--- logical-viewer/logical-viewer/App.xaml.cs
+++ logical-viewer/logical-viewer/App.xaml.cs
@@ -1,28 +1,6 @@
-﻿// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
+﻿using Microsoft.UI.Xaml;
 
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace logical_viewer
+namespace LogicalViewer
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
@@ -42,7 +20,7 @@ namespace logical_viewer
         /// Invoked when the application is launched.
         /// </summary>
         /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
             m_window = new MainWindow();
             m_window.Activate();
