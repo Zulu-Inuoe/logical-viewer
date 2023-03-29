b347044c79ca762a63d5622d57e4ed1c28a91fae
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2023-03-29 10:00:01 -0400

    Update view/vm for timeago strings on last sync

--- logical-viewer/logical-viewer/ViewModel/UserLogVM.cs
+++ logical-viewer/logical-viewer/ViewModel/UserLogVM.cs
@@ -1,11 +1,41 @@
 ﻿using LogicalParse.DataModel;
 using LogicalViewer.Util;
+using System.Collections.Generic;
 
 namespace LogicalViewer.ViewModel
 {
     public class UserLogVM : ViewModelBase
     {
+        public UserLog UserLog
+        {
+            get => m_UserLog;
+            set
+            {
+                if (m_UserLog != value)
+                {
+                    m_UserLog = value;
+
+                    UserSyncQueues.Clear();
+                    if (m_UserLog != null)
+                    {
+                        foreach (var queue in m_UserLog.UserSyncQueues)
+                        {
+                            UserSyncQueues.Add(new()
+                            {
+                                UserSyncQueue = queue
+                            });
+                        }
+                    }
+                }
+            }
+        }
+
         public string Name { get; set; }
-        public UserLog UserLog { get; set; }
+        public List<UserAccount> UserAccounts => UserLog.UserAccounts;
+        public List<UserCalendar> UserCalendars => UserLog.UserCalendars;
+        public List<UserSyncQueueVM> UserSyncQueues { get; set; } = new();
+        public UserCalendarSet CurrentCalendarSet => UserLog.CurrentCalendarSet;
+
+        private UserLog m_UserLog;
     }
 }
---
+++ logical-viewer/logical-viewer/ViewModel/UserSyncQueueVM.cs
@@ -0,0 +1,20 @@
+﻿using Humanizer;
+using LogicalParse.DataModel;
+using LogicalViewer.Util;
+using System;
+
+namespace LogicalViewer.ViewModel
+{
+    public class UserSyncQueueVM : ViewModelBase
+    {
+        public UserSyncQueue UserSyncQueue { get; set; }
+
+        public string SourceKind => UserSyncQueue.SourceKind;
+        public string SourceID => UserSyncQueue.SourceID;
+        public string? SourceUser => UserSyncQueue.SourceUser;
+        public DateTime LastSync => UserSyncQueue.LastSync;
+        public string LastSyncAgo => LastSync.Humanize();
+        public string QueueKind => UserSyncQueue.QueueKind;
+        public bool Active => UserSyncQueue.Active;
+    }
+}
