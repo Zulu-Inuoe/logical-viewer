7b49b2413ffa8123d0612e878425a632b84853e8
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2023-03-30 09:32:26 -0400

    Right-click copy of cells
    This is so awful. Need to figure out how to abstract this out to reuse same copy command/flyout while passing a CommandParameter w/ the text.

--- logical-viewer/logical-viewer/View/UserLogPage.xaml.cs
+++ logical-viewer/logical-viewer/View/UserLogPage.xaml.cs
@@ -1,5 +1,7 @@
 using LogicalViewer.ViewModel;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Windows.ApplicationModel.DataTransfer;
 
 namespace LogicalViewer.View
 {
@@ -10,6 +12,33 @@ namespace LogicalViewer.View
         public UserLogPage()
         {
             this.InitializeComponent();
+        }
+
+        private void CopyableTextBlock_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            var elt = sender as TextBlock;
+            if (elt is null)
+            {
+                return;
+            }
+
+            var flyout = elt.ContextFlyout as MenuFlyout;
+            if (flyout is null)
+            {
+                flyout = new();
+                elt.ContextFlyout = flyout;
+            }
+
+            var copyCommand = new StandardUICommand(StandardUICommandKind.Copy);
+            copyCommand.ExecuteRequested += (_, _) =>
+            {
+                var dataPackage = new DataPackage() { RequestedOperation = DataPackageOperation.Copy };
+                dataPackage.SetText(elt.Text);
+                Clipboard.SetContent(dataPackage);
+            };
+
+            var copyFlyout = new MenuFlyoutItem() { Command = copyCommand };
+            flyout.Items.Add(copyFlyout);
         }
     }
 }
